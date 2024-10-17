83ee75c2fdd2da3dfec1618d2d7883c66b44e1cf
pasfono <pasfono@protonmail.com>
2024-10-17 20:19:23 +0800

    ticker colored

--- xbt-2100/src/app/components/Ticker/Ticker.js
+++ xbt-2100/src/app/components/Ticker/Ticker.js
@@ -74,21 +74,15 @@ function Ticker() {
         <div ref={scrollRef} className={style.hero_scroll_content}>
           {items.concat(items).map((item, index) => (
             <span key={index} className="">
-              {/* <img
-                  className={style.hero_ticker_img}
-                  src="/assets/img/vitalik-clapping.gif"
-                  alt=""
-                /> */}
               {typeof item === "string" ? (
-                <>
-                  <div className={style.hero_written_container}>
-                    <div>{item}</div>
+                <div className={style.hero_written_container}>
+                  <div style={{ color: item.includes("+") ? "green" : item.includes("-") ? "red" : "white" }}>
+                    {item}
                   </div>
-                </>
+                </div>
               ) : (
                 <img
                   className={style.hero_ticker_img}
-                  // src="/buyvideo.gif"
                   src={item.src}
                   alt=""
                 />
